955e47814c86f723d28008518394b15ae6e3ca25
zkrough26 <70219015+zkrough26@users.noreply.github.com>
2023-04-05 22:59:01 -0400

    Added initial .ino file
    This code is currently working.  It transmits 512 8-bit sensor readings once every second.  No FFT is calculated yet.  The ADC will also have to be set to 16-bit readings prior to implementing the FFT.

---
+++ TempSensCode.ino
@@ -0,0 +1,64 @@
+#include "SparkFun_SinglePairEthernet.h"
+
+#define ChipSelPin CS
+#define InterruptPin G1
+#define ResetPin G0
+
+SinglePairEthernet adin1110;
+
+byte deviceMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0x03};
+byte destinationMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0xFF};
+
+byte dataBuffer[512];
+int counter;
+unsigned long lastBlink = 0;
+
+void setup() 
+{
+    pinMode(A0, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    analogReadResolution(8);
+    delay(1000);
+    if (!adin1110.begin(deviceMAC, LED_BUILTIN, InterruptPin, ResetPin, ChipSelPin)) 
+    {
+      while(1)
+      {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(100);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(100);
+      }
+    }
+    while (adin1110.getLinkStatus() != true);
+}
+
+void loop() {   
+    dataBuffer[counter] = analogRead(A0);
+    counter++;
+    if (counter == 512)
+    {
+      sendSensData();
+    }
+    delayMicroseconds(1953);
+
+    unsigned long now = millis();
+    if((now - lastBlink >= 1000) && (adin1110.getLinkStatus()))
+    {
+        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); 
+        lastBlink = now;
+    }
+    if(!adin1110.getLinkStatus())
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+}
+
+void sendSensData()
+{
+  if(adin1110.getLinkStatus())
+  {
+    adin1110.sendData(dataBuffer, sizeof(dataBuffer), destinationMAC);
+  }
+  memset(dataBuffer, 0, sizeof(dataBuffer));
+  counter = 0;
+}
