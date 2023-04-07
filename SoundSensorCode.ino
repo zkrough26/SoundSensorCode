1fb3248e5ef3456e3563d84ebd775f6dd176150d
zkrough26 <70219015+zkrough26@users.noreply.github.com>
2023-04-07 19:27:20 -0400

    Added FFT Functionality to Sensor Code
    The FFT files were included and an FFT object was created.  The sound sampling rate was changes to sample at 10kHz instead of 512Hz.  The transmission speed is controlled in the sendSensData() function now to ensure we are not sending data to the RSA too fast.

--- SoundSensorCode.ino
+++ SoundSensorCode.ino
@@ -1,15 +1,18 @@
 #include "SparkFun_SinglePairEthernet.h"
+#include "DAT_FFT_App.h"
 
 #define ChipSelPin CS
 #define InterruptPin G1
 #define ResetPin G0
 
 SinglePairEthernet adin1110;
+DAD_FFT_App fftApp;
 
 byte deviceMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0x03};
 byte destinationMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0xCA};
 
-byte dataBuffer[512];
+uint16_t dataBuffer[512];
+byte outputBuffer[512];
 int counter;
 unsigned long lastBlink = 0;
 
@@ -17,8 +20,8 @@ void setup()
 {
     pinMode(A0, INPUT);
     pinMode(LED_BUILTIN, OUTPUT);
-    analogReadResolution(8);
-    delay(1000);
+    analogReadResolution(16);
+    delay(500);
     if (!adin1110.begin(deviceMAC, LED_BUILTIN, InterruptPin, ResetPin, ChipSelPin)) 
     {
       while(1)
@@ -37,9 +40,10 @@ void loop() {
     counter++;
     if (counter >= 512)
     {
+      calculateFFT();
       sendSensData();
     }
-    delay(4);
+    delayMicroSeconds(100);
 
     unsigned long now = millis();
     if((now - lastBlink >= 1000) && (adin1110.getLinkStatus()))
@@ -47,18 +51,21 @@ void loop() {
         digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); 
         lastBlink = now;
     }
-    if(!adin1110.getLinkStatus())
-    {
-      digitalWrite(LED_BUILTIN, LOW);
-    }
+}
+
+void calculateFFT()
+{
+  fftApp.FFT_Calculate((float *)dataBuffer, outputBuffer);
+  memset(dataBuffer, 0, sizeof(dataBuffer))
 }
 
 void sendSensData()
 {
   if(adin1110.getLinkStatus())
   {
-    adin1110.sendData(dataBuffer, sizeof(dataBuffer), destinationMAC);
+    adin1110.sendData(outputBuffer, sizeof(outputBuffer), destinationMAC);
   }
-  memset(dataBuffer, 0, sizeof(dataBuffer));
+  memset(outputBuffer, 0, sizeof(outputBuffer));
   counter = 0;
+  delay(25000);
 }
