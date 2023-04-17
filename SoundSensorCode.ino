ab6b3faf4357247c903caa8c6428195482787b6b
zkrough26 <70219015+zkrough26@users.noreply.github.com>
2023-04-17 00:20:52 -0400

    Updated to use new FFT Algo
    We switched from the initial FFT algorithm to a new and updated version.  This version works much better and gives proper FFT outputs.

--- SoundSensorCode.ino
+++ SoundSensorCode.ino
@@ -1,80 +1,106 @@
 #include "SparkFun_SinglePairEthernet.h"
-#include "DAD_FFT_App.h"
+#include "arduinoFFT.h"
 
+// Config Defines
+// #define DEBUG_MODE
+
+#ifndef DEBUG_MODE
 #define ChipSelPin CS
 #define InterruptPin G1
 #define ResetPin G0
+#endif
 
 SinglePairEthernet adin1110;
-DAD_FFT_App fftApp;
+arduinoFFT FFT;
 
-byte deviceMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0x03};
-byte destinationMAC[6] = {0x00, 0xE0, 0x22, 0xFE, 0xDA, 0xCA};
+byte deviceMAC[6] = { 0x00, 0xE0, 0x22, 0xFE, 0xDA, 0x03 };
+byte destinationMAC[6] = { 0x00, 0xE0, 0x22, 0xFE, 0xDA, 0xCA };
 
-float dataBuffer[512];
-byte outputBuffer[512];
-int counter;
+const uint16_t samples = 1024;
+const double samplingFrequency = 10000;
+double vReal[samples];
+double vImag[samples];
+byte outputBuffer[samples / 2];
+
 unsigned long lastBlink = 0;
 
-void setup() 
-{
-    pinMode(A0, INPUT);
-    pinMode(LED_BUILTIN, OUTPUT);
-    analogReadResolution(16);
-    delay(500);
-    if (!adin1110.begin(deviceMAC, LED_BUILTIN, InterruptPin, ResetPin, ChipSelPin)) 
-    {
-      while(1)
-      {
-        digitalWrite(LED_BUILTIN, HIGH);
-        delay(100);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(100);
-      }
+void setup() {
+#ifdef DEBUG_MODE
+  Serial.begin(9600);
+#endif
+
+  pinMode(A0, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  analogReadResolution(8);
+  delay(500);
+#ifndef DEBUG_MODE
+  if (!adin1110.begin(deviceMAC, LED_BUILTIN, InterruptPin, ResetPin, ChipSelPin)) {
+    while (1) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(100);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(100);
     }
-    while (adin1110.getLinkStatus() != true);
+  }
+  while (adin1110.getLinkStatus() != true)
+    ;
+#endif
 }
 
-void loop() {   
-    collectData(dataBuffer);
-    calculateFFT();
-    sendSensData();
+void loop() {
+  collectData();
+  calculateFFT();
+  sendSensData();
 
-    unsigned long now = millis();
-    if((now - lastBlink >= 1000) && (adin1110.getLinkStatus()))
-    {
-        digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN)); 
-        lastBlink = now;
-    }
+#ifndef DEBUG_MODE
+  unsigned long now = millis();
+  if ((now - lastBlink >= 1000) && (adin1110.getLinkStatus())) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    lastBlink = now;
+  }
+#endif
 }
 
-void collectData(float sample[SAMPLE_LENGTH]){
-  long int delay1, delay2, delay3, delay4; 
-  for(int i = 0; i < SAMPLE_LENGTH; i++){
-    sample[i] = analogRead(A0);    
+void collectData() {
+  for (int i = 0; i < samples; i++) {
+    vImag[i] = 0;
+  }
+  long int delay1, delay2, delay3, delay4;
+  for (int i = 0; i < samples; i++) {
+    vReal[i] = analogRead(A0);
     delayMicroseconds(19);
-    delay1++; delay2++; delay3++; delay4++; // delayMicroseconds only has so much resolution. Incrementing these arificially brings us closer to the targetted delay
-  }
-
-  // Remove DC offset
-  for(int i = 0; i < SAMPLE_LENGTH; i++){
-    sample[i] = sample[i] - 32857.36;    
+    delay1++;
+    delay2++;
+    delay3++;
+    delay4++;
   }
 }
 
-void calculateFFT()
-{
-  fftApp.FFT_Calculate(dataBuffer, outputBuffer);
-  memset(dataBuffer, 1, sizeof(dataBuffer));
+void calculateFFT() {
+  FFT = arduinoFFT(vReal, vImag, samples, samplingFrequency); /* Create FFT object */
+  FFT.DCRemoval();
+  FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
+  FFT.Compute(FFT_FORWARD);                        /* Compute FFT */
+  FFT.ComplexToMagnitude();                        /* Compute magnitudes */
 }
 
-void sendSensData()
-{
-  if(adin1110.getLinkStatus())
-  {
+void sendSensData() {
+  for (int i = 0; i < samples/2; i++) {
+    outputBuffer[i] = (byte)vReal[i];
+
+#ifdef DEBUG_MODE
+    Serial.print(outputBuffer[i]);
+    Serial.print(" ");
+#endif
+  }
+#ifdef DEBUG_MODE
+  Serial.println();
+#endif
+
+#ifndef DEBUG_MODE
+  if (adin1110.getLinkStatus()) {
     adin1110.sendData(outputBuffer, sizeof(outputBuffer), destinationMAC);
   }
-  memset(outputBuffer, 0, sizeof(outputBuffer));
-  counter = 0;
-  delay(1000);
+#endif
+  delay(3500);
 }
